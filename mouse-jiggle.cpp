2a74fbd38d16eae3ab82f60d67c6d1e384eeb305
alija <alijakoe@gmail.com>
2025-07-22 21:48:26 +0700

    adjust implementation to utilize mouse input instead of using set cursor directly

--- mouse-jiggle.cpp
+++ mouse-jiggle.cpp
@@ -1,24 +1,53 @@
 #include <windows.h>
-#include <iostream>
 #include <thread>
 #include <chrono>
+#include <iostream>
 
-int main()
-{
-    std::cout << "Press Ctrl+C to stop.\n";
+void simulateMouseActivity(int move) {
+    INPUT input = { 0 };
+    input.type = INPUT_MOUSE;
+    input.mi.dx = move;
+    input.mi.dy = move;
+    input.mi.dwFlags = MOUSEEVENTF_MOVE;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void simulateMouseClick() {
+    INPUT inputs[2] = {};
+
+    // Mouse left button down
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+    // Mouse left button up
+    inputs[1].type = INPUT_MOUSE;
+    inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+    SendInput(2, inputs, sizeof(INPUT));
+}
+
+int main() {
+    int move = 1;
+    int interval = 1;
+
+    std::cout << "pixel move: ";
+    std::cin >> move;
+    std::cout << "interval (s): ";
+    std::cin >> interval;
+    std::cout << "press [ctrl+c] to stop.\n";
 
     while (true) {
-        POINT cursorPos;
-        GetCursorPos(&cursorPos);
+        if (move > 0)
+        {
+            move *= -1;
+        }
+        else
+        {
+            move = std::abs(move);
+        }
 
-        // Move the cursor 1px right and back
-        SetCursorPos(cursorPos.x + 1, cursorPos.y);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        SetCursorPos(cursorPos.x, cursorPos.y);
-
-        // Wait 5 seconds before next wiggle
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+        simulateMouseActivity(move);
+        std::this_thread::sleep_for(std::chrono::seconds(interval));
     }
-
     return 0;
 }
